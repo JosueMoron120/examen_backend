37f98ddc6e60c2991565026a6ef138c3cb0d5cbf
Josué Morón <josuemoron@upeu.edu.pe>
2021-11-18 19:55:15 -0500

    primera prueba

---
+++ src/config/default.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+module.exports = {
+    database : {
+        host :  `ec2-3-209-38-221.compute-1.amazonaws.com`,
+        user : `nqvnuyufomjxed`,
+        password : `e18805832f0994d8728978a6d473271b0b6cfa171d050f70a064d1ed153de000`,
+        database : `dfagi48rno6rch`,
+        port : `5432`,
+        ssl : {
+            rejectUnauthorized : false
+        }
+
+
+    }, 
+  
+
+
+    
+}
